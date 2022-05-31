85f5834d9bb28e837997f67e740869fd7d9bd164
Tiago Costa <tiagodpv@gmail.com>
2022-05-31 10:06:07 -0400

    monsters

--- 06_Monster/Assets/Scripts/Player.cs
+++ 06_Monster/Assets/Scripts/Player.cs
@@ -19,14 +19,24 @@ public class Player : MonoBehaviour
 
     private Animator anim;
     private string WALK_ANIMATION = "Walk";
-    
-    // Start is called before the first frame update
-    void Start()
+
+    private bool isGrounded;
+    private string GROUND_TAG = "Ground";
+
+    private string ENEMY_TAG = "Enemy";
+
+    private void Awake()
     {
         myBody = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
 
-        sr = GetComponent<SpriteRenderer>();        
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         
     }
 
     // Update is called once per frame
@@ -68,10 +78,28 @@ public class Player : MonoBehaviour
 
     void PlayerJump()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            myBody.AddForce(new Vector2)
+            isGrounded = false;
+            myBody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag(GROUND_TAG))
+            isGrounded = true;
+        
+
+        if (collision.gameObject.CompareTag(ENEMY_TAG))        
+            Destroy(gameObject);
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(ENEMY_TAG))
+            Destroy(gameObject);
     }
 
 
